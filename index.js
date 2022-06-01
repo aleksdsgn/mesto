c13c23b5177f23df044401aff75648c0d19cdf64
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-06-01 18:55:31 +0700

    feat: реализовать открытие и закрытие попапа на js

---
+++ index.js
@@ -0,0 +1,38 @@
+const profileEditButton = document.querySelector('.profile__button_type_edit'); // переименовать имя класса в html
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
+
+function closePopupOnQ(e) {
+  if (e.code === 'KeyQ') {
+    closePopup();
+  }
+  console.log(1);
+}
+
+function openPopup() {
+  popup.classList.remove('popup_hidden');
+  document.addEventListener('keypress', closePopupOnQ);
+}
+
+function closePopup() {
+  popup.classList.add('popup_hidden');
+  document.removeEventListener('keypress', closePopupOnQ);
+}
+
+profileEditButton.addEventListener('click', function() {
+  openPopup()
+});
+
+popupCloseButton.addEventListener('click', function() {
+  closePopup()
+});
+
+popup.addEventListener('click', function(e) {
+  if (e.target === e.currentTarget) {
+    closePopup();
+  }
+});
+
+document.addEventListener('keypress', function(e) {
+
+})
