8e55eec731e060900d410417582427074f9f5e00
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-06-02 22:23:49 +0700

    feat: реализовать ввод в инпут в js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close-button');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close-button');
+
+let nameInput = popup.querySelector('.popup__name-input');
+let jobInput = popup.querySelector('.popup__job-input');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
 
 function closePopupOnQ(e) {
-  if (e.code === 'KeyQ') {
+  if (e.code === 'Escape') {
     closePopup();
   }
 }
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  document.addEventListener('keypress', closePopupOnQ);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  document.addEventListener('keyup', closePopupOnQ);
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('keypress', closePopupOnQ);
+  document.removeEventListener('keyup', closePopupOnQ);
 }
 
 profileEditButton.addEventListener('click', function() {
@@ -32,31 +39,11 @@ popup.addEventListener('click', function(e) {
   }
 });
 
-let nameInput = popup.querySelector('.popup__name-input');
-let jobInput = popup.querySelector('.popup__job-input');
-// let profileName = document.getElementsByClassName('profile__title').textContent;
-let profileJob = document.getElementsByClassName('profile__subtitle');
-let profileName = document.querySelector('.profile__title');
-
-// console.log(nameInput.value);
-// console.log(jobInput);
-console.log(profileName.textContent);
-// console.log(profileName);
-
-// console.log(profileJob);
-
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 popup.addEventListener('submit', formSubmitHandler);
