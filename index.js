7905f4d26bba831d08fd9c001205d506b58c9d18
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-06-02 18:03:51 +0700

    feat: input  в процессе разработки

--- index.js
+++ index.js
@@ -31,3 +31,32 @@ popup.addEventListener('click', function(e) {
     closePopup();
   }
 });
+
+let nameInput = popup.querySelector('.popup__name-input');
+let jobInput = popup.querySelector('.popup__job-input');
+// let profileName = document.getElementsByClassName('profile__title').textContent;
+let profileJob = document.getElementsByClassName('profile__subtitle');
+let profileName = document.querySelector('.profile__title');
+
+// console.log(nameInput.value);
+// console.log(jobInput);
+console.log(profileName.textContent);
+// console.log(profileName);
+
+// console.log(profileJob);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+popup.addEventListener('submit', formSubmitHandler);
