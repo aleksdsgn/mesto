4a58be30141339462329a6dbfab31e4df8cf9df7
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-06-27 22:51:44 +0700

    feat: реализовать валидацию полей ввода для попапов

--- validate.js
+++ validate.js
@@ -1,3 +1,76 @@
+// показать ошибку
+const showInputError = (formElement, inputElement, errorMessage, setting) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(setting.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(setting.errorClass);
+};
+
+// спрятать ошибку
+const hideInputError = (formElement, inputElement, setting) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(setting.inputErrorClass);
+  errorElement.classList.remove(setting.errorClass);
+  errorElement.textContent = '';
+};
+
+// проверка на валидность
+const checkInputValidity = (formElement, inputElement, setting) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, setting);
+  } else {
+    hideInputError(formElement, inputElement, setting);
+  }
+};
+
+// проверяет есть ли не валидные поля
+// возможно вместо input нужно inputElement
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+// поведение кнопки сабмита если есть невалидный инпут
+const toggleButtonState = (inputList, buttonElement, setting) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(setting.inactiveButtonClass);
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove(setting.inactiveButtonClass);
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+// установка слушателей на каждый инпут
+const setEventListeners = (formElement, setting) => {
+  const inputList = Array.from(formElement.querySelectorAll(setting.inputSelector));
+  const buttonElement = formElement.querySelector(setting.submitButtonSelector);
+
+  // чтобы проверить состояние кнопки в самом начале
+  toggleButtonState(inputList, buttonElement, setting);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, setting);
+
+      // чтобы проверять при изменении любого из полей
+      toggleButtonState(inputList, buttonElement, setting);
+    });
+  });
+};
+
+// валидация всех форм
+const enableValidation = (setting) => {
+  const formList = Array.from(document.querySelectorAll(setting.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault;
+    });
+    setEventListeners(formElement, setting);
+  });
+};
+
 // включение валидации всех форм
 enableValidation({
   formSelector: '.popup__form',
@@ -7,5 +80,3 @@ enableValidation({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible',
 });
-
-
