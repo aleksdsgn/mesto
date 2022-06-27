126dccbb376ed453aab007528e41e6599495456f
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-06-27 14:06:38 +0700

    feat: добавить разметку под валидацию

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 // редактирования профиля
-const profileEditButton = document.querySelector(".profile__button_type_edit"); // кнопка открытия редактирования профиля
-const popupEditProfile = document.querySelector(".popup_type_edit-profile"); // селектор попапа редактирования профиля
+const profileEditButton = document.querySelector('.profile__button_type_edit'); // кнопка открытия редактирования профиля
+const popupEditProfile = document.querySelector('.popup_type_edit-profile'); // селектор попапа редактирования профиля
 
-const nameInput = popupEditProfile.querySelector(".popup__input_type_name"); // поле ввода имени в профиле
-const jobInput = popupEditProfile.querySelector(".popup__input_type_job"); // поле ввода доп. информации в профиле
+const nameInput = popupEditProfile.querySelector('.popup__input_type_name'); // поле ввода имени в профиле
+const jobInput = popupEditProfile.querySelector('.popup__input_type_job'); // поле ввода доп. информации в профиле
 
-const profileName = document.querySelector(".profile__title"); // отображаемое в разметке имя профиля
-const profileJob = document.querySelector(".profile__subtitle"); // отображаемая в разметке доп. информация
+const profileName = document.querySelector('.profile__title'); // отображаемое в разметке имя профиля
+const profileJob = document.querySelector('.profile__subtitle'); // отображаемая в разметке доп. информация
 
 // добавления карточки
-const profileAddButton = document.querySelector(".profile__button_type_add"); // кнопка открытия добавления карточки
-const popupAddCard = document.querySelector(".popup_type_add-card"); // селектор попапа добавления карточки
+const profileAddButton = document.querySelector('.profile__button_type_add'); // кнопка открытия добавления карточки
+const popupAddCard = document.querySelector('.popup_type_add-card'); // селектор попапа добавления карточки
 
-const titleInput = popupAddCard.querySelector(".popup__input_type_title"); // поле ввода заголовка карточки
-const linkInput = popupAddCard.querySelector(".popup__input_type_link"); // поле ввода ссылки на картинку
+const titleInput = popupAddCard.querySelector('.popup__input_type_title'); // поле ввода заголовка карточки
+const linkInput = popupAddCard.querySelector('.popup__input_type_link'); // поле ввода ссылки на картинку
 
-const cardTitle = document.querySelector(".card__title"); // отображаемое в карточке название
-const cardImage = document.querySelector(".card__image"); // отображаемая в карточке картинка
+const cardTitle = document.querySelector('.card__title'); // отображаемое в карточке название
+const cardImage = document.querySelector('.card__image'); // отображаемая в карточке картинка
 
-const placesContainer = document.querySelector(".places__container"); // тег ul внутрь которого вставляются шаблонные li
-const cardTemplate = document.querySelector("#card-template").content; // поиск шаблона для карточки
+const placesContainer = document.querySelector('.places__container'); // тег ul внутрь которого вставляются шаблонные li
+const cardTemplate = document.querySelector('#card-template').content; // поиск шаблона для карточки
 
 // открытие картинки
-const popupOpenImg = document.querySelector(".popup_type_img"); // селектор попапа открытия картинки
-const popupImage = document.querySelector(".popup__image"); // картинка в попапе
-const popupCaption = document.querySelector(".popup__caption"); // подпись к картинке в попапе
+const popupOpenImg = document.querySelector('.popup_type_img'); // селектор попапа открытия картинки
+const popupImage = document.querySelector('.popup__image'); // картинка в попапе
+const popupCaption = document.querySelector('.popup__caption'); // подпись к картинке в попапе
 
 //
 // ------------функции---------------------------------------------------------
 //
   // ф-я открытия всех попапов
 function openPopup(namePopup) {
-  namePopup.classList.add("popup_opened");
-  document.addEventListener("keydown", closePopupOnEsc); // событие закрытия по Escape
+  namePopup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupOnEsc); // событие закрытия по Escape
 }
 
   // ф-я закрытия всех попапов
 function closePopup(namePopup) {
-  namePopup.classList.remove("popup_opened");
-  document.removeEventListener("keydown", closePopupOnEsc); // событие закрытия по Escape
+  namePopup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupOnEsc); // событие закрытия по Escape
 }
 
 //функция закрытия всех попапов по клавише Escape
 function closePopupOnEsc(evt) {
-  if (evt.key === "Escape") {
+  if (evt.key === 'Escape') {
     console.log(`Нажали на кнопку ${evt.key}`);
-    const openedPopup = document.querySelector(".popup_opened");
+    const openedPopup = document.querySelector('.popup_opened');
     closePopup(openedPopup);
   }
 }
@@ -61,10 +61,10 @@ function handleProfileFormSubmit(e) {
   closePopup(popupEditProfile);
 }
 
-popupEditProfile.addEventListener("submit", handleProfileFormSubmit); // событие отправки введенных данных редактирования профиля на страницу
+popupEditProfile.addEventListener('submit', handleProfileFormSubmit); // событие отправки введенных данных редактирования профиля на страницу
 
   // событие открытия редактирования профиля
-profileEditButton.addEventListener("click", function () {
+profileEditButton.addEventListener('click', function () {
   nameInput.value = profileName.textContent; // выводим что было написано ранее
   jobInput.value = profileJob.textContent;
   openPopup(popupEditProfile);
@@ -76,27 +76,27 @@ profileEditButton.addEventListener("click", function () {
   // ф-я создания новой карточки
 function createCard(name, link) {
   const newElement = cardTemplate.cloneNode(true); // клонирование содержимого шаблона
-  const cardTitle = newElement.querySelector(".card__title");
-  const cardImage = newElement.querySelector(".card__image");
+  const cardTitle = newElement.querySelector('.card__title');
+  const cardImage = newElement.querySelector('.card__image');
 
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
 
   newElement
-    .querySelector(".card__button-like")
+    .querySelector('.card__button-like')
       // переключатель лайка
-    .addEventListener("click", (evt) => {
-      evt.target.classList.toggle("card__button-like_active");
+    .addEventListener('click', (evt) => {
+      evt.target.classList.toggle('card__button-like_active');
     });
   newElement
-    .querySelector(".card__button-delete")
+    .querySelector('.card__button-delete')
       // удаляем карточку
-    .addEventListener("click", (evt) => {
-      evt.target.closest(".places__element").remove();
+    .addEventListener('click', (evt) => {
+      evt.target.closest('.places__element').remove();
     });
     // открытие картинки
-    cardImage.addEventListener("click", () => {
+    cardImage.addEventListener('click', () => {
     renderImg(name, link);
     openPopup(popupOpenImg);
   });
@@ -125,10 +125,10 @@ function hundleAddCardSubmit(evt) {
   evt.target.reset(); // очищаем поля формы
 }
 
-popupAddCard.addEventListener("submit", hundleAddCardSubmit); // событие добавления карточки
+popupAddCard.addEventListener('submit', hundleAddCardSubmit); // событие добавления карточки
 
   // событие открытия добавления карточки
-profileAddButton.addEventListener("click", function () {
+profileAddButton.addEventListener('click', function () {
   openPopup(popupAddCard);
 });
 
@@ -143,11 +143,11 @@ function renderImg(name, link) {
 }
 
   // ф-я закрытия всех модалок по клику вне окна или по Х
-document.querySelectorAll(".popup").forEach((popup) => {
-  popup.addEventListener("mousedown", (evt) => {
+document.querySelectorAll('.popup').forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
     if (
       evt.target === evt.currentTarget ||
-      evt.target.classList.contains("popup__close-button")
+      evt.target.classList.contains('popup__close-button')
     ) {
       closePopup(popup);
     }
---
+++ validate.js
@@ -0,0 +1,11 @@
+// включение валидации всех форм
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+});
+
+
